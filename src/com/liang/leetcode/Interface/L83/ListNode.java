55316d43edea8fd104e8941da5205a96d644607a
-LiaNg- <shlycsd@163.com>
2018-11-03 15:53:11 +0800

    删除排序链表中的重复元素

--- src/com/liang/leetcode/Interface/L83/ListNode.java
+++ src/com/liang/leetcode/Interface/L83/ListNode.java
@@ -10,26 +10,24 @@ public class ListNode {
 
     public int val;
     public ListNode next;
-    public ListNode(int x) {
+
+    ListNode(int x) {
         val = x;
     }
 
-    public static ListNode buildListNode(int[] input){
-        ListNode first = null,last = null,newNode;
-        int num;
-        if(input.length>0){
-            for(int i=0;i<input.length;i++){
-                newNode=new ListNode(input[i]);
-                newNode.next=null;
-                if(first==null){
-                    first=newNode;
-                    last=newNode;
+    public static ListNode buildListNode(int[] input) {
+        ListNode first = null, last = null, newNode;
+        if (input.length > 0) {
+            for (int i = 0; i < input.length; i++) {
+                newNode = new ListNode(input[i]);
+                newNode.next = null;
+                if (first == null) {
+                    first = newNode;
+                    last = newNode;
+                } else {
+                    last.next = newNode;
+                    last = newNode;
                 }
-                else{
-                    last.next=newNode;
-                    last=newNode;
-                }
-
             }
         }
         return first;
