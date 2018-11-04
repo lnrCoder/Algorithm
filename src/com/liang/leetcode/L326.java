8881557bc31569f4870ce0ee184f5d24d584499e
-LiaNg- <shlycsd@163.com>
2018-11-04 11:50:42 +0800

    L326
    Signed-off-by: -LiaNg- <shlycsd@163.com>

--- src/com/liang/leetcode/L326.java
+++ src/com/liang/leetcode/L326.java
@@ -8,4 +8,18 @@ package com.liang.leetcode;
  */
 public class L326 {
 
+    public static void main(String[] args) {
+        L326 l = new L326();
+        boolean result = l.isPowerOfThree(27);
+        System.out.println(result);
+    }
+
+    /**
+     * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+     * 不使用循环或者递归
+     */
+    public boolean isPowerOfThree(int n) {
+        double result = Math.log10(n) / Math.log10(3);
+        return (result == (int) result) ? true : false;
+    }
 }
