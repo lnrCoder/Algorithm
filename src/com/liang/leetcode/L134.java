ee179f5890ad58ba1416a3fd0d121798df66371f
-LiaNg- <shlycsd@163.com>
2018-11-07 17:13:58 +0800

    L275

---
+++ src/com/liang/leetcode/L134.java
@@ -0,0 +1,56 @@
+package com.liang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName L134
+ * @description gas-station
+ * @Author levo
+ * @Date 2018/11/7
+ */
+public class L134 {
+
+
+    public static void main(String[] args) {
+
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+
+        L134 l = new L134();
+        System.out.println(l.canCompleteCircuit(gas, cost));
+    }
+
+    /**
+     * 加油站
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int length = gas.length;
+
+        for (int i=0;i<length;i++) {
+            if(cost[i]>gas[i]){
+                continue;
+            }
+            int startGas = gas[i];
+            int nowIndex = i;
+            for(int j = i;j<length+i;j++){
+                startGas = startGas - cost[nowIndex];
+
+                if(startGas<0){
+                    break;
+                }
+                nowIndex = nowIndex + 1;
+                if(nowIndex>=length){
+                    nowIndex = 0;
+                }
+                if(nowIndex == i){
+                    return i;
+                }
+                startGas = startGas+gas[nowIndex];
+            }
+        }
+
+        return -1;
+    }
+}
