2a4bc1ac8338b72d11dd3bc0c4ff1b08f984fdc6
-LiaNg- <shlycsd@163.com>
2018-12-06 11:10:09 +0800

    L205

--- src/com/liang/leetcode/L205.java
+++ src/com/liang/leetcode/L205.java
@@ -1,10 +1,5 @@
 package com.liang.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import javax.management.StringValueExp;
-
 /**
  * @ClassName L205
  * @description isomorphic-strings
@@ -21,20 +16,23 @@ public class L205 {
         System.out.println(l.isIsomorphic(s, t));
     }
 
+    /**
+     * 给定两个字符串 s 和 t，判断它们是否是同构的。
+     * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+     * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+     */
     public boolean isIsomorphic(String s, String t) {
 
-        if(s.length()==0){
+        if (s.length() == 0) {
             return true;
         }
 
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
-        int i=0;
+        int i = 0;
 
-        while(i<s.length())
-        {
-            if(s.indexOf(String.valueOf(sChars[i]))!=t.indexOf(String.valueOf(tChars[i])))
-            {
+        while (i < s.length()) {
+            if (s.indexOf(String.valueOf(sChars[i])) != t.indexOf(String.valueOf(tChars[i]))) {
                 return false;
             }
             i++;
