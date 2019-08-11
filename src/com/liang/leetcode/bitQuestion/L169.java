ea97e58717178e5a6f3e56ea2d86cb23c9d4b3ea
-LiaNg- <shlycsd@163.com>
2019-08-11 10:16:01 +0800

    位运算求众数

---
+++ src/com/liang/leetcode/bitQuestion/L169.java
@@ -0,0 +1,35 @@
+package com.liang.leetcode.bitQuestion;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName L169
+ * @description majority-element 求众数
+ * @Author LiaNg
+ * @Date 2019-08-10
+ */
+public class L169 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,2,2,3};
+        int i = majorityElement(nums);
+        System.out.println("i = " + i);
+    }
+
+    private static int majorityElement(int[] nums) {
+        int result = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+            }
+            if (num == result) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return result;
+    }
+}
