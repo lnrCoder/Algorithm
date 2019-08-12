ed1363e9ab2ec460e00a5271d9bba8ffecdaebeb
-LiaNg- <shlycsd@163.com>
2019-08-12 09:45:54 +0800

    颠倒二进制位

---
+++ src/com/liang/leetcode/bitQuestion/L190.java
@@ -0,0 +1,27 @@
+package com.liang.leetcode.bitQuestion;
+
+/**
+ * @ClassName L190
+ * @description reverse-bits 颠倒二进制位
+ * @Author LiaNg
+ * @Date 2019-08-12
+ */
+public class L190 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int reverseBits = reverseBits(n);
+        System.out.println("reverseBits = " + reverseBits);
+    }
+
+    private static int reverseBits(int n) {
+
+        int result = 0;
+        for(int i = 0; i<31;i++){
+            result  += n&1;
+            n>>=1;
+            result<<=1;
+        }
+        return result + (n & 1);
+    }
+}
