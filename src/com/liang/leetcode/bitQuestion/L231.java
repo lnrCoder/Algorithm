4a14e333a63877f1340115eaacdc4199d7d35ef1
-LiaNg- <shlycsd@163.com>
2019-08-12 08:34:17 +0800

    位运算 2的幂

---
+++ src/com/liang/leetcode/bitQuestion/L231.java
@@ -0,0 +1,20 @@
+package com.liang.leetcode.bitQuestion;
+
+/**
+ * @ClassName L231
+ * @description power-of-two 2的幂
+ * @Author LiaNg
+ * @Date 2019-08-12
+ */
+public class L231 {
+
+    public static void main(String[] args) {
+        int n = 8;
+        boolean powerOfTwo = isPowerOfTwo(n);
+        System.out.println("powerOfTwo = " + powerOfTwo);
+    }
+
+    private static boolean isPowerOfTwo(int n) {
+        return n>0 && ((n&(n-1))==0);
+    }
+}
