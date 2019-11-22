5fe97f1c3e929c13ca7fa72d9d61e4bd2e71f02e
lnrCoder <shlycsd@163.com>
2019-11-22 16:10:41 +0800

    LeetCode - stack - 856.括号的分数

--- src/com/liang/leetcode/stack/L856.java
+++ src/com/liang/leetcode/stack/L856.java
@@ -45,5 +45,30 @@ public class L856 {
         return stack.pop();
     }
 
+    /**
+     * LeetCode 耗时最短解答
+     */
+    public int scoreOfParentheses1(String S) {
+        return F(S, 0, S.length());
+    }
 
+    public int F(String S, int i, int j) {
+        //Score of balanced string S[i:j]
+        int ans = 0, bal = 0;
+
+        // Split string into primitives
+        for (int k = i; k < j; ++k) {
+            bal += S.charAt(k) == '(' ? 1 : -1;
+            if (bal == 0) {
+                if (k - i == 1) {
+                    ans++;
+                } else {
+                    ans += 2 * F(S, i + 1, k);
+                }
+                i = k + 1;
+            }
+        }
+
+        return ans;
+    }
 }
