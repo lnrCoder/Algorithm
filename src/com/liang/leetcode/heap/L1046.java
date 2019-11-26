8558962e8daff6fb4544919cef2c0a0a743abea3
lnrCoder <shlycsd@163.com>
2019-11-26 09:27:12 +0800

    LeetCode - heap - 1046. 最后一块石头的重量

--- src/com/liang/leetcode/heap/L1046.java
+++ src/com/liang/leetcode/heap/L1046.java
@@ -12,11 +12,20 @@ public class L1046 {
 
     public static void main(String[] args) {
         int[] input = {2, 7, 4, 1, 8, 1};
-
         L1046 l1046 = new L1046();
         System.out.println("l1046.lastStoneWeight(input) = " + l1046.lastStoneWeight(input));
     }
 
+    /**
+     * 有一堆石头，每块石头的重量都是正整数。
+     * 每一回合，从中选出两块最重的石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+     * 如果 x == y，那么两块石头都会被完全粉碎；
+     * 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+     * 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/last-stone-weight
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public int lastStoneWeight(int[] stones) {
 
         if (stones.length == 1) {
@@ -39,6 +48,6 @@ public class L1046 {
             }
         }
 
-        return pq.size() > 0 ? pq.poll() : 0;
+        return pq.isEmpty() ? 0 : pq.poll();
     }
 }
