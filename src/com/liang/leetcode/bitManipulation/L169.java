400997e1c6a0b84dd617a7559f1c51f17b8f0156
lnrcoder <shlycsd@163.com>
2020-02-02 10:02:49 +0800

    LeetCode - bitManipulation - 169. 多数元素

--- src/com/liang/leetcode/bitManipulation/L169.java
+++ src/com/liang/leetcode/bitManipulation/L169.java
@@ -2,7 +2,7 @@ package com.liang.leetcode.bitManipulation;
 
 /**
  * @ClassName L169
- * @description majority-element 求众数
+ * @description majority-element 多数元素
  * @Author LiaNg
  * @Date 2019-08-10
  */
@@ -10,11 +10,18 @@ public class L169 {
 
     public static void main(String[] args) {
 
-        int[] nums = {1,1,2,2,3};
+        int[] nums = {1, 1, 2, 2, 3};
         int i = majorityElement(nums);
         System.out.println("i = " + i);
     }
 
+    /**
+     * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/majority-element
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     private static int majorityElement(int[] nums) {
         int result = 0;
         int count = 0;
