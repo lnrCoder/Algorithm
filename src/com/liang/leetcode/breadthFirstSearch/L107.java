f861673e101459de54bde7a4f3defdcfc7041278
lnrcoder <shlycsd@163.com>
2020-03-05 00:13:34 +0800

    LeetCode - BFS - 107. 二叉树的层次遍历 II

---
+++ src/com/liang/leetcode/breadthFirstSearch/L107.java
@@ -0,0 +1,54 @@
+package com.liang.leetcode.breadthFirstSearch;
+
+import java.util.*;
+
+/**
+ * @ClassName: L107
+ * @Description: 二叉树的层次遍历 II
+ * @Author: LiaNg
+ * @Date: 2020/3/4 22:40
+ */
+public class L107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        if(root==null){
+            return resultList;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+
+            List<Integer> subList = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            resultList.add(subList);
+        }
+
+        Collections.reverse(resultList);
+        return resultList;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
