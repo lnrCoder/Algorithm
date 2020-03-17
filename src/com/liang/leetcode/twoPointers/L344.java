45b6397fa364fa152fd77f9aa3fde50a6e77f1d1
lnrcoder <shlycsd@163.com>
2020-03-17 19:56:42 +0800

    LeetCode - 双指针 - 344. 反转字符串

--- src/com/liang/leetcode/twoPointers/L344.java
+++ src/com/liang/leetcode/twoPointers/L344.java
@@ -11,12 +11,20 @@ import java.util.Arrays;
 public class L344 {
 
     public static void main(String[] args) {
-        char[] s = new char[]{'h','e','l','l','o'};
+        char[] s = new char[]{'h', 'e', 'l', 'l', 'o'};
         L344 l344 = new L344();
         l344.reverseString(s);
         System.out.println("s = " + Arrays.toString(s));
     }
 
+    /**
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O (1) 的额外空间解决这一问题。
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-string
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public void reverseString(char[] s) {
         int left = 0;
         int right = s.length - 1;
@@ -28,7 +36,7 @@ public class L344 {
 //            s[left] = s[right];
 //            s[right] = tmp;
 
-            s[left]^=s[right];
+            s[left] ^= s[right];
             s[right] ^= s[left];
             s[left] ^= s[right];
             left++;
