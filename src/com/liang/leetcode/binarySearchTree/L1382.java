b987ff58c141aee6392dd2e0b5648529bc61e07d
lnrcoder <shlycsd@163.com>
2020-03-26 13:22:29 +0800

    LeetCode - 二叉搜索树 - 1382. 将二叉搜索树变平衡

--- src/com/liang/leetcode/binarySearchTree/L1382.java
+++ src/com/liang/leetcode/binarySearchTree/L1382.java
@@ -11,6 +11,18 @@ import java.util.List;
  */
 public class L1382 {
 
+    /**
+     * 给你一棵二叉搜索树，请你返回一棵 平衡后 的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。
+     * 如果一棵二叉搜索树中，每个节点的两棵子树高度差不超过 1 ，我们就称这棵二叉搜索树是 平衡的 。
+     * 如果有多种构造方法，请你返回任意一种。
+     *  
+     * 提示：
+     * 树节点的数目在 1 到 10^4 之间。
+     * 树节点的值互不相同，且在 1 到 10^5 之间。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/balance-a-binary-search-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public TreeNode balanceBST(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         inorderTraversal(root, list);
