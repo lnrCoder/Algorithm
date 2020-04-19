c7c6ff289e8959e98a87db46e9d74973e8c5fbaf
lnrcoder <shlycsd@163.com>
2020-04-20 01:40:34 +0800

    LeetCode - 每日一练 - 200. 岛屿数量

---
+++ src/com/liang/leetcode/daily/L200.java
@@ -0,0 +1,79 @@
+package com.liang.leetcode.daily;
+
+/**
+ * @ClassName: L200
+ * @Description: 岛屿数量
+ * @Author: LiaNg
+ * @Date: 2020/4/20 1:26
+ */
+public class L200 {
+
+    public static void main(String[] args) {
+        L200 l200 = new L200();
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println("l200.numIslands(grid) = " + l200.numIslands(grid));
+    }
+
+    int[][] indexArr = new int[][]{
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+
+    /**
+     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和 / 或竖直方向上相邻的陆地连接形成。
+     * 此外，你可以假设该网格的四条边均被水包围。
+     * 示例 1:
+     * 输入:
+     * 11110
+     * 11010
+     * 11000
+     * 00000
+     * 输出: 1
+     * 示例 2:
+     * 输入:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     * 输出: 3
+     * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/number-of-islands
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int numIslands(char[][] grid) {
+        int res = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    bfs(i, j, grid);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public void bfs(int i, int j, char[][] grid) {
+
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        for (int[] index : indexArr) {
+            bfs(i + index[0], j + index[1], grid);
+        }
+    }
+}
