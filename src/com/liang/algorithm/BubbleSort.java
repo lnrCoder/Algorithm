bb587357f143f5df12d7ca0b1fb9b1857d3cc461
lnrcoder <shlycsd@163.com>
2020-04-26 15:33:02 +0800

    冒泡排序 Java 实现

--- src/com/liang/algorithm/BubbleSort.java
+++ src/com/liang/algorithm/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.liang.algorithm;
 
+import java.util.Arrays;
+
 /**
  * @ClassName BubbleSort
  * @description 冒泡排序
@@ -9,42 +11,44 @@ package com.liang.algorithm;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] numbers = {3, 5, 4, 1, 2, 6};
+        int[] numbers = {14, 2, 25, 17, 7, 38, 34, 5, 42, 15, 42};
 
         BubbleSort bs = new BubbleSort();
         bs.bubbleSort(numbers, numbers.length);
+        System.out.println("numbers = " + Arrays.toString(numbers));
     }
 
     /**
      * 冒泡排序
      *
-     * @description 逐步两两比较，大的值放后面
      * @param numbers 数组
      * @param size 数组大小
+     * @description 逐步两两比较，大的值放后面
      */
     public void bubbleSort(int[] numbers, int size) {
+
         if (size <= 1) {
             return;
         }
         for (int i = 0; i < size; ++i) {
-            // 提前退出冒泡循环的标志位
-            boolean flag = false;
+            // 数据交换的标记，表示当前循环是否有数据交换
+            boolean swapFlag = false;
             for (int j = 0; j < size - i - 1; ++j) {
+                // 相邻位置两两比较
                 if (numbers[j] > numbers[j + 1]) {
-                    // 交换
-                    int tmp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = tmp;
-                    // 表示有数据交换
-                    flag = true;
+                    // 前面数字大于后面，则交换位置
+                    numbers[j] ^= numbers[j + 1];
+                    numbers[j+1] ^= numbers[j];
+                    numbers[j] ^= numbers[j + 1];
+                    // 本次循环有数据交换，标记设为 true
+                    swapFlag = true;
                 }
             }
-            // 没有数据交换，提前退出
-            if (!flag) {
+
+            if (!swapFlag) {
+                // 没有数据交换，提前退出
                 break;
             }
         }
     }
-
-
 }
