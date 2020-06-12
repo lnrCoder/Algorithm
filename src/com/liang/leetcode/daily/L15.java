e468973282ad140d3f973fc9437f05439e44ef6f
lnrcoder <shlycsd@163.com>
2020-06-12 14:26:55 +0800

    feat(LeetCode): 每日一题
    15 三数之和

--- src/com/liang/leetcode/daily/L15.java
+++ src/com/liang/leetcode/daily/L15.java
@@ -1,6 +1,7 @@
 package com.liang.leetcode.daily;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,10 +14,29 @@ public class L15 {
 
     public static void main(String[] args) {
         L15 l15 = new L15();
-        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        int[] nums = new int[]{-2, 0, 1, 1, 2};
+        System.out.println("l15.threeSum(nums) = " + l15.threeSum(nums));
     }
 
+    /**
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     * 注意：答案中不可以包含重复的三元组。
+     *  
+     * 示例：
+     * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+     * 满足要求的三元组集合为：
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/3sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
         List<List<Integer>> res = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -26,26 +46,28 @@ public class L15 {
 
             int target = -nums[i];
             int j = i + 1;
-            int k = nums.length - 1 ;
+            int k = nums.length - 1;
 
             while (j < k) {
                 if (nums[j] + nums[k] == target) {
                     List<Integer> currentList = new ArrayList<>();
-                    currentList.add(i);
-                    currentList.add(j);
-                    currentList.add(k);
+                    currentList.add(nums[i]);
+                    currentList.add(nums[j]);
+                    currentList.add(nums[k]);
+                    res.add(currentList);
                     j++;
-                    k++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (k > j && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
                 } else if (nums[j] + nums[k] > target) {
                     k--;
-                }else{
+                } else {
                     j++;
-                }
-                while (j < k && nums[j] == nums[j - 1]) {
-                    j++;
-                }
-                while (k > j && nums[k] == nums[k - 1]) {
-                    k--;
                 }
             }
         }
