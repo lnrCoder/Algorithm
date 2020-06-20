ef8ecaf325cfdeb37c036093a37cb7bb0662dabc
lnrcoder <shlycsd@163.com>
2020-06-21 07:52:27 +0800

    feat(LeetCode): 每日一题
    124 二叉树中的最大路径和

---
+++ src/com/liang/leetcode/daily/L124.java
@@ -0,0 +1,71 @@
+package com.liang.leetcode.daily;
+
+/**
+ * 二叉树中的最大路径和
+ *
+ * @author LiaNg
+ * @date 2020/6/21 7:50
+ */
+public class L124 {
+
+    int max = Integer.MIN_VALUE;
+
+    /**
+     * 给定一个非空二叉树，返回其最大路径和。
+     * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+     * 示例 1:
+     * 输入: [1,2,3]
+     *       1
+     *      / \
+     *     2   3
+     * 输出: 6
+     * 示例 2:
+     * 输入: [-10,9,20,null,null,15,7]
+     *    -10
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 输出: 42
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    /**
+     * 返回经过root的单边分支最大和， 即Math.max(root, root+left, root+right)
+     *
+     * @param root root
+     * @return int
+     */
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //计算左边分支最大值，左边分支如果为负数还不如不选择
+        int leftMax = Math.max(0, dfs(root.left));
+        //计算右边分支最大值，右边分支如果为负数还不如不选择
+        int rightMax = Math.max(0, dfs(root.right));
+        //left->root->right 作为路径与历史最大值做比较
+        max = Math.max(max, root.val + leftMax + rightMax);
+        // 返回经过root的单边最大分支给上游
+        return root.val + Math.max(leftMax, rightMax);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
