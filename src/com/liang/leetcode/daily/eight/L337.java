4775632c836d77055833472f7a6db21d76c9d6d0
lnrcoder <shlycsd@163.com>
2020-08-05 09:15:55 +0800

    feat(LeetCode): 每日一题
    337 打家劫舍 III

---
+++ src/com/liang/leetcode/daily/eight/L337.java
@@ -0,0 +1,65 @@
+package com.liang.leetcode.daily.eight;
+
+/**
+ * 打家劫舍 III
+ *
+ * @author LiaNg
+ * @date 2020/8/5 9:08
+ */
+public class L337 {
+
+    /**
+     * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+     * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+     * 示例 1:
+     * 输入: [3,2,3,null,3,null,1]
+     * 3
+     * / \
+     * 2   3
+     * \   \
+     * 3   1
+     * 输出: 7
+     * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+     * 示例 2:
+     * 输入: [3,4,5,1,3,null,1]
+     *      3
+     * / \
+     * 4   5
+     * / \   \
+     * 1   3   1
+     * 输出: 9
+     * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/house-robber-iii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int rob(TreeNode root) {
+        int[] result = robInternal(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    public int[] robInternal(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] result = new int[2];
+
+        int[] left = robInternal(root.left);
+        int[] right = robInternal(root.right);
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = left[0] + right[0] + root.val;
+
+        return result;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
