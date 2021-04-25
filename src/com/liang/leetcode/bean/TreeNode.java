517f930834802c3b7a4693139a9cf1afb6ca5308
lnrcoder <shlycsd@163.com>
2021-04-25 10:12:27 +0800

    TreeNode 基础类

---
+++ src/com/liang/leetcode/bean/TreeNode.java
@@ -0,0 +1,25 @@
+package com.liang.leetcode.bean;
+
+/**
+ * @author LiaNg
+ * @date 2021/4/25 10:11
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
